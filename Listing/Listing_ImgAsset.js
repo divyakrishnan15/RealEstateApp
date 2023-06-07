114eca7a332f50bb8ad7ae2ed95ccd5d10d6ddb7
divyakrishnan15 <divyabalajee@gmail.com>
2023-06-07 11:54:40 -0400

    listing deploy

--- Listing/Listing_ImgAsset.js
+++ Listing/Listing_ImgAsset.js
@@ -1,61 +1,61 @@
 const imagedata = {
   "5dba1fb94aa4055b9f296970": [
-    "./ListingAssets/1/front.jpg",
-    "./ListingAssets/1/living.jpg",
-    "./ListingAssets/1/bedroom.jpg",
+    "./Listing/ListingAssets/1/front.jpg",
+    "./Listing/ListingAssets/1/living.jpg",
+    "./Listing/ListingAssets/1/bedroom.jpg",
   ],
 
   "5dba1fb94aa4055b9f296949": [
-    "./ListingAssets/2/front1.png",
-    "./ListingAssets/2/living.jpg",
-    "./ListingAssets/2/bedroom.jpg",
+    "./Listing/ListingAssets/2/front.png",
+    "./Listing/ListingAssets/2/living.jpg",
+    "./Listing/ListingAssets/2/bedroom.jpg",
   ],
 
   "5dba1fb94aa4055b9f2969b2": [
-    "./ListingAssets/3/front.png",
-    "./ListingAssets/3/living.jpg",
-    "./ListingAssets/3/bedroom.jpg",
+    "./Listing/ListingAssets/3/front.png",
+    "./Listing/ListingAssets/3/living.jpg",
+    "./Listing/ListingAssets/3/bedroom.jpg",
   ],
 
   "5dba1fb94aa4055b9f2969a0": [
-    "./ListingAssets/4/front.jpg",
-    "./ListingAssets/4/living.jpg",
-    "./ListingAssets/4/bedroom.jpg",
+    "./Listing/ListingAssets/4/front.jpg",
+    "./Listing/ListingAssets/4/living.jpg",
+    "./Listing/ListingAssets/4/bedroom.jpg",
   ],
 
   "5dba1fb94aa4055b9f296984": [
-    "./ListingAssets/5/front.jpg",
-    "./ListingAssets/5/living.jpg",
-    "./ListingAssets/5/bedroom.jpg",
+    "./Listing/ListingAssets/5/front.jpg",
+    "./Listing/ListingAssets/5/living.jpg",
+    "./Listing/ListingAssets/5/bedroom.jpg",
   ],
 
   "5dba1fb94aa4055b9f29695c": [
-    "./ListingAssets/6/front.jpg",
-    "./ListingAssets/6/living.jpg",
-    "./ListingAssets/6/bedroom.jpg",
+    "./Listing/ListingAssets/6/front.jpg",
+    "./Listing/ListingAssets/6/living.jpg",
+    "./Listing/ListingAssets/6/bedroom.jpg",
   ],
 
   "5dba1fba4aa4055b9f296a95": [
-    "./ListingAssets/7/front.png",
-    "./ListingAssets/7/living.png",
-    "./ListingAssets/7/bedroom.jpg",
+    "./Listing/ListingAssets/7/front.png",
+    "./Listing/ListingAssets/7/living.png",
+    "./Listing/ListingAssets/7/bedroom.jpg",
   ],
 
   "5dba1fba4aa4055b9f296a32": [
-    "./ListingAssets/8/front.png",
-    "./ListingAssets/8/living.jpg",
-    "./ListingAssets/8/bedroom.jpg",
+    "./Listing/ListingAssets/8/front.png",
+    "./Listing/ListingAssets/8/living.jpg",
+    "./Listing/ListingAssets/8/bedroom.jpg",
   ],
 
   "5dba1fba4aa4055b9f296a53": [
-    "./ListingAssets/9/front.jpg",
-    "./ListingAssets/9/living.jpg",
-    "./ListingAssets/9/bedroom.jpg",
+    "./Listing/ListingAssets/9/front.jpg",
+    "./Listing/ListingAssets/9/living.jpg",
+    "./Listing/ListingAssets/9/bedroom.jpg",
   ],
 
   "5dba1fba4aa4055b9f296aa1": [
-    "./ListingAssets/10/front.jpg",
-    "./ListingAssets/10/living.jpg",
-    "./ListingAssets/10/bedroom.jpg",
+    "./Listing/ListingAssets/10/front.jpg",
+    "./Listing/ListingAssets/10/living.jpg",
+    "./Listing/ListingAssets/10/bedroom.jpg",
   ],
 };
